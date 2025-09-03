22aa44ad2dafb4f44584761914284182e8e9c67e
storyteller24 <161675929+storyteller24@users.noreply.github.com>
2025-09-03 14:00:26 +0700

    disable projectile when hit

---
+++ Sunny Day/Assets/Script/ProjectileBehavior.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class EProjectileBehavior : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            gameObject.SetActive(false); // Deactivate the projectile
+        }
+        else if (collision.CompareTag("Obstacles"))
+        {
+            gameObject.SetActive(false); // Deactivate the projectile
+            Debug.Log("Hit an obstacle");
+        }
+    }
+    void OnBecameInvisible()
+    {
+        gameObject.SetActive(false); // return to pool instead if needed
+    }
+}
